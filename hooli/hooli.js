508161ef02bbf9f60819a7c293abe5d4da79b648
Edmund Cheng <ec3219@nyu.edu>
2020-10-18 20:21:32 -0400

    added grid

--- hooli/hooli.js
+++ hooli/hooli.js
@@ -1,10 +1,1 @@
 console.log('Hello')
-
-var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
