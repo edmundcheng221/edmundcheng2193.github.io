f9c7d5c68d6b6a8a2b12a1185067c09a29232cd9
Edmund Cheng <ec3219@nyu.edu>
2020-11-09 16:36:13 -0500

    added mcluhan reading response

--- mcluhan/mcluhan.js
+++ mcluhan/mcluhan.js
@@ -6,8 +6,18 @@ const theTxt = document.querySelector('p');
 const text= ["I found the text interesting. When McLuhan talks about the Medium, he is referring to the way in which messages are transmitted. I particularly like the part where McLuhan says'Many of our institutions suppress althe natural direct experience of youth, who respond with untaught delight to the poetry and the beauty of the new technological environment.' (page 100) Perhaps McLuhan is trying to argue that people should be allowed to experience new technology on their own. I also like how there were a lot of pictures because the reading is way too long."]
 
 let i = 0;
+document.getElementsById('switching').addEventListener("click", imagefunction);
+
 
 function clicker(){    
     theTxt.textContent = text[i];
+    document.getElementById('picture').src="../img/mcluhan_tech.png"
+
 }
+
+// document.getElementsById('switching').addEventListener("click", imagefunction);
+// function imagefunction(){
+//     document.getElementById('picture').src="../img/mcluhan_tech.png"
+
+//     }
 
