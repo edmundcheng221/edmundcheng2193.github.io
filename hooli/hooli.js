1c3fc27e4d9c6ec61d84e000f5e071165f874024
Edmund Cheng <ec3219@nyu.edu>
2020-11-17 15:22:14 -0500

    fixed modal close function

--- hooli/hooli.js
+++ hooli/hooli.js
@@ -2,3 +2,17 @@ console.log('Hello World')
 console.log('Will add some JavaScript soon.....')
 console.log('I am a developer who clearly can\'t write JavaScript. Call me a failure.')
 
+
+
+let modal = document.getElementById('id01');
+let modal2 = document.getElementById('id02');
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+
+  } else if (event.target == modal2) {
+    modal2.style.display = "none";
+      
+  }
+}
