1c1dde76ec00557fc2131805a4ae4232257d39cb
Edmund Cheng <ec3219@nyu.edu>
2020-12-08 16:03:26 -0500

    added memos and articles to homepage

--- HHM/HHM.js
+++ HHM/HHM.js
@@ -1,1 +1,23 @@
-console.log('testing')
+function random_binary_search(arr, item){
+    if (arr.length < 1) {
+        return None } 
+    let begin = 0;
+    let end = arr.length - 1;
+    while (begin < end) {
+        let midpoint = begin + Math.floor(((end-begin)/2));
+        let val = arr[midpoint];
+        if (val === item) {
+            return midpoint }
+        else if (val < item) {
+            end = midpoint - 1;
+
+        }
+        else {
+            begin = midpoint + 1;
+        }
+    }
+    return None
+
+}
+
+console.log(random_binary_search([1, 5, 6, 8, 2], 6))
