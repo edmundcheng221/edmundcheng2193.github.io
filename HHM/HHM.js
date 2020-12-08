f40bc9134227e9c3bea33da830b1a85d8f57e53e
Edmund Cheng <ec3219@nyu.edu>
2020-12-08 16:07:49 -0500

    fixed bug

--- HHM/HHM.js
+++ HHM/HHM.js
@@ -1,26 +1,27 @@
-function binary_search(arr, item){
-    if (arr.length < 1) {
-        return None } 
-    else if (arr.length === 1 && arr[0] === item) {
-        return arr[0]
-    }
-    let begin = 0;
-    let end = arr.length - 1;
-    while (begin < end) {
-        let midpoint = begin + Math.floor(((end-begin)/2));
-        let val = arr[midpoint];
-        if (val === item) {
-            return midpoint }
-        else if (val < item) {
-            end = midpoint - 1;
+console.log('Hello World')
+// function binary_search(arr, item){
+//     if (arr.length < 1) {
+//         return None } 
+//     else if (arr.length === 1 && arr[0] === item) {
+//         return arr[0]
+//     }
+//     let begin = 0;
+//     let end = arr.length - 1;
+//     while (begin < end) {
+//         let midpoint = begin + Math.floor(((end-begin)/2));
+//         let val = arr[midpoint];
+//         if (val === item) {
+//             return midpoint }
+//         else if (val < item) {
+//             end = midpoint - 1;
 
-        }
-        else {
-            begin = midpoint + 1;
-        }
-    }
-    return None
+//         }
+//         else {
+//             begin = midpoint + 1;
+//         }
+//     }
+//     return None
 
-}
+// }
 
-console.log(binary_search([1, 5, 6, 8, 2], 6))
+// console.log(binary_search([1, 5, 6, 8, 2], 6))
