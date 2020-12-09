3b07fc6137fb1619a22207443ab4b84af3550134
Edmund Cheng <ec3219@nyu.edu>
2020-12-09 17:10:11 -0500

    added translation

--- HHM/contact.js
+++ HHM/contact.js
@@ -9,31 +9,37 @@ function validate(){
     error_message.style.padding = "10px";
     
     let text;
+
     if(name.length < 1){
       text = "Please Enter valid Name";
       error_message.innerHTML = text;
       return false;
     }
+    
     if(subject.length < 1){
       text = "Please Enter Correct Subject";
       error_message.innerHTML = text;
       return false;
     }
+
     if(isNaN(phone) || phone.length != 10){
       text = "Please Enter valid Phone Number";
       error_message.innerHTML = text;
       return false;
     }
+
     if(email.includes('@') == false || email.includes('.') == false) {
       text = "Please Enter valid Email";
       error_message.innerHTML = text;
       return false;
     }
+
     if(message.length <= 2){
       text = "Please Enter a Message";
       error_message.innerHTML = text;
       return false;
     }
+
     alert("Thank you for your message! We will get back to you soon.");
     return true;
   }
